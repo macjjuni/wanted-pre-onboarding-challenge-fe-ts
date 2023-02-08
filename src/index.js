dff86086df245cfe8915b102856b1757cb156e0c
junheelee <macjjuni@gmail.com>
2023-02-08 17:23:20 +0900

    1-1 사전과제 리팩토링 결과

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@
 /** @version 1.0 */
 
 /**
+ * Todo Tags 주요 객체
+ * 
+ * @class
+ * @property {string} id 				ID 식별자(require)
+ * @property {string} title 	  Tag 타이틀(require)
+ */
+class Tag {}
+
+/**
  * Todo 주요 객체
  * 
  * @class
@@ -9,9 +18,10 @@
  * @property {string} content 	내용(require)
  * @property {boolean} isDone 	완료여부(require)
  * @property {string} category 	카테고리(optional)
- * @property {string[]} tag 		태그(optional)
+ * @property {Tag[]} Tags 		태그(optional)
  */
 class Todo {}
+
 
 /**
  * 할 일을 추가할 수 있다.
@@ -19,7 +29,7 @@ class Todo {}
  * @construcor
  * @param {string} content - (require)
  * @param {string} category - (optional)
- * @param {string[]} tag - (optional)
+ * @param {Tag[]} tag - (optional)
  * @returns { }
  */
 const addTodo = () => {}
@@ -40,19 +50,35 @@ const getTodo = () => {}
  * @param {string} content - (require)
  * @param {string} isDone - (require)
  * @param {string} category - (optional)
- * @param {string} tag - (optional)
+ * @param {Tag[]} tag - (optional)
  * @returns {void}
  */
-const editTodo = () => {}
-
+const editTodo = (comtent, isDone, category, tag) => {}
 
 /**
  * ID를 기반으로 특정 할 일을 삭제할 수 있다.
- * 모든 할 일을 제거할 수 있다.
- * 특정 할 일의 특정 태그를 삭제할 수 있다.
- * 특정 할 일의 모든 태그를 제거할 수 있다.
  * @construcor
  * @param {string} id - (require)
  * @returns {void}
  */
 const delTodo = (id) => {}
+
+/**
+ * 모든 할 일을 제거할 수 있다.
+ * @construcor
+ * @returns {void}
+ */
+const delAllTodo = () => {}
+
+/**
+ * 특정 할 일의 특정 태그를 삭제할 수 있다.
+ * @param {string} id 
+ * @param {string[]} tagId 
+ */
+const delTodoTagsById = (id, tagId) => {}
+
+/**
+ * 특정 할 일의 모든 태그를 제거할 수 있다.
+ * @param {string} id 
+ */
+const delTodoAllTags = (id) => {}
